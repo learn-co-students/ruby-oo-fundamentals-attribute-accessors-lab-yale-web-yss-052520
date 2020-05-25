1d80a67a6bae996d9497b9e4bf170ded636b73de
edensongjbs <edensongjbs@gmail.com>
2020-05-25 17:45:09 -0400

    Done.

---
+++ lib/cat.rb
@@ -0,0 +1,10 @@
+class Cat
+    # def initialize(cat_name)
+    #     @name=name
+    # end
+    attr_accessor :name
+    def meow
+        puts "meow!"
+    end
+end
+
--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,10 @@
 ## code your solution here. 
+class Cat
+    def initialize(cat_name="cat")
+         @name=name
+    end
+    attr_accessor :name
+    def meow
+        puts "meow!"
+    end
+end
